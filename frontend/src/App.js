3a621380366757d22c2768ea9315dbfb19f7e435
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2024-03-01 15:41:55 +0530

    Remove unnecessary words & comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,9 +23,9 @@ function App() {
     }
   };
 
-  const addTask = async (title, isFavorite) => { // Modify addTask function to accept isFavorite parameter
+  const addTask = async (title) => {  //addTask function
     try {
-      const response = await axios.post(`${SERVER_URL}/api/tasks`, { title, favorite: isFavorite }); // Pass isFavorite in the request body
+      const response = await axios.post(`${SERVER_URL}/api/tasks`, { title});
       setTasks([...tasks, response.data]);
     } catch (error) {
       console.error('Error adding task:', error);
@@ -36,12 +36,12 @@ function App() {
     setEditTaskData(task);
   };
 
-  const updateTask = async (id, title, isFavorite) => { // Modify updateTask function to accept isFavorite parameter
+  const updateTask = async (id, title) => { //updateTask function
     try {
-      await axios.patch(`${SERVER_URL}/api/tasks/${id}`, { title, favorite: isFavorite }); // Pass isFavorite in the request body
+      await axios.patch(`${SERVER_URL}/api/tasks/${id}`, { title });
       const updatedTasks = tasks.map(task => {
         if (task._id === id) {
-          return { ...task, title, favorite: isFavorite }; // Update favorite status
+          return { ...task, title };
         }
         return task;
       });
@@ -51,8 +51,17 @@ function App() {
       console.error('Error updating task:', error);
     }
   };
-  
-  const toggleTaskCompletion = async (id) => {
+
+  const deleteTask = async (id) => {
+    try {
+      await axios.delete(`${SERVER_URL}/api/tasks/${id}`);
+      setTasks(tasks.filter(task => task._id !== id));
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
+ const toggleTaskCompletion = async (id) => {  //Compelted Tasks
     try {
       const updatedTasks = tasks.map(task => {
         if (task._id === id) {
@@ -67,16 +76,7 @@ function App() {
     }
   };
 
-  const deleteTask = async (id) => {
-    try {
-      await axios.delete(`${SERVER_URL}/api/tasks/${id}`);
-      setTasks(tasks.filter(task => task._id !== id));
-    } catch (error) {
-      console.error('Error deleting task:', error);
-    }
-  };
-
-  const toggleFavorite = async (id) => {
+  const toggleFavorite = async (id) => {  //Favourites Tasks
     try {
       const updatedTasks = tasks.map(task => {
         if (task._id === id) {
@@ -93,8 +93,8 @@ function App() {
 
   return (
     <div className="container mx-auto mt-8 px-4 ">
-      <div class="text-5xl font-sans font-medium tracking-wide text-center shadow-md p-4 mb-4">
-        <span class=" bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-violet-600">To-Do List</span>
+      <div className="text-5xl font-sans font-medium tracking-wide text-center shadow-md p-4 mb-4">
+        <span className=" bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-violet-600">To-Do List</span>
       </div>      
       {/* <h1 className="font-sans font-medium tracking-wide text-4xl text-center shadow-md p-4 mb-4">To-Do List</h1> */}
       <TaskForm serverUrl={SERVER_URL} addTask={addTask} editTaskData={editTaskData} updateTask={updateTask} />
